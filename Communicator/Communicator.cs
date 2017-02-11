019bd7c6e2c8d1c6552cb9302c07799e40a40df8
Saikat Modak <saikat.modak@monotype.com>
2017-02-12 00:04:25 +0530

    Basic ListenChannel implemented

---
+++ Communicator/Communicator.cs
@@ -0,0 +1,88 @@
+﻿using PubNubMessaging.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+  public interface IMessageProcessor
+  {
+    void ProcesssReceivedMessage(string contact, string message);
+    void ProcessSentStatus(string result);
+  }
+
+  public class Communicator
+  {
+    //Public Section
+    public Communicator(IMessageProcessor listener, string pubKey = null, string subKey = null)
+    {
+      mListener = listener;
+      mPubnub = new Pubnub(PUBLISHKEY, SUBSCRIBEKEY);
+      mWaitHandle = new ManualResetEvent(false);
+      if (!String.IsNullOrEmpty(subKey))
+        SUBSCRIBEKEY = subKey;
+      if (!String.IsNullOrEmpty(pubKey))
+        PUBLISHKEY = pubKey;
+    }
+
+    public bool ListenChannel(string channel)
+    {
+      mPubnub.Subscribe<string>(
+        channel,
+        SubscribeCallback,
+        ConnectCallback,
+        ErrorCallback);
+      bool ret = mWaitHandle.WaitOne(mTimeout);
+      mWaitHandle.Reset();
+      return ret;
+    }
+
+    private void ErrorCallback(PubnubClientError obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void ConnectCallback(string obj)
+    {
+      //throw new NotImplementedException();
+      mWaitHandle.Set();
+    }
+
+    //Private Section
+    private void SubscribeCallback(string obj)
+    {
+      Console.WriteLine("SUBSCRIBE REGULAR CALLBACK:");
+      Console.WriteLine(obj);
+      string resultActualMessage = "";
+      string resultContact = "";
+      if (!string.IsNullOrEmpty(obj) && !string.IsNullOrEmpty(obj.Trim()))
+      {
+        List<object> deserializedMessage = mPubnub.JsonPluggableLibrary.DeserializeToListOfObject(obj);
+        if (deserializedMessage != null && deserializedMessage.Count > 0)
+        {
+          object subscribedObject = (object)deserializedMessage[0];
+          if (subscribedObject != null)
+          {
+            resultActualMessage = subscribedObject.ToString();
+          }
+          subscribedObject = (object)deserializedMessage[2];
+          if (subscribedObject != null)
+          {
+            resultContact = subscribedObject.ToString();
+          }
+        }
+        mListener.ProcesssReceivedMessage(resultContact, resultActualMessage);
+      }
+    }
+
+    private readonly string PUBLISHKEY = "pub-c-221f8e24-5c10-4f7d-972d-2ebded5abdff";
+    private readonly string SUBSCRIBEKEY = "sub-c-7495c98c-de01-11e6-989b-02ee2ddab7fe";
+    private IMessageProcessor mListener;
+    private Pubnub mPubnub;
+    private const int mTimeout = 2000;
+    private ManualResetEvent mWaitHandle;
+  }
+}
---
+++ TestDirectChat/TestCommunicator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
+using PubNubMessaging.Core;
+using System.Threading;
+
+namespace TestCloudMessagingApps
+{
+  class TestObserver : IMessageProcessor
+  {
+    public void ProcessSentStatus(string result)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void ProcesssReceivedMessage(string channel, string message)
+    {
+      Channel = channel;
+      Message = message;
+      ev.Set();
+    }
+
+    public TestObserver(ManualResetEvent ev)
+    {
+      this.ev = ev;
+    }
+
+    public string Channel {get; set;}
+    public string Message { get; set;}
+    private ManualResetEvent ev;
+  }
+
+  [TestClass]
+  public class TestCommunicator
+  {
+    private const string PUBLISHKEY = "pub-c-221f8e24-5c10-4f7d-972d-2ebded5abdff";
+    private const string SUBSCRIBEKEY = "sub-c-7495c98c-de01-11e6-989b-02ee2ddab7fe";
+    private Pubnub pubnub;
+    private ManualResetEvent ev;
+    private const int timeout = 2000;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+      pubnub = new Pubnub(PUBLISHKEY, SUBSCRIBEKEY);
+      ev = new ManualResetEvent(false);
+    }
+
+    [TestMethod]
+    public void ListenChannel_Successful()
+    {
+      TestObserver ob = new TestObserver(ev);
+      Communicator c = new Communicator(ob);
+
+      string channel = "999";
+      string message = "hi";
+
+      Assert.IsTrue(c.ListenChannel(channel));
+
+      var res = pubnub.Publish<string>(
+          channel,
+          message,
+          DisplayReturnMessage,
+          DisplayErrorMessage
+          );
+      res = ev.WaitOne(timeout);
+      ev.Reset();
+      Assert.IsTrue(res);
+      Assert.AreEqual(message, ob.Message);
+    }
+
+    private void DisplayErrorMessage(PubnubClientError obj)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void DisplayReturnMessage(string obj)
+    {
+    }
+  }
+}
