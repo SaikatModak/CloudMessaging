5c9cf04066503c12dd8b24869a7e703047382053
SaikatModak <saikat.modak19@gmail.com>
2017-02-12 21:43:26 +0530

    Implemented blank Tray App

---
+++ DirectChat/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectChat
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      WindowState = WindowState.Minimized;
+
+      NotifyIcon NIcon = new NotifyIcon();
+      NIcon.Visible = true;
+      NIcon.Icon = new Icon(@"icon.ico");
+      NIcon.Click += (o, e) =>
+      {
+        if (IsVisible)
+        {
+          Hide();
+        }
+        else
+        {
+          Show();
+          WindowState = WindowState.Normal;
+        }
+      };
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      e.Cancel = true;
+      Hide();
+    }
+  }
+}
